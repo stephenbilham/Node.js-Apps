7bacb19d676bcdb413f5949ff1179b5f3b782cdd
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-03-27 14:00:03 -0700

    adding handlebars to render dynamic pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,8 +5,17 @@ const app = express();
 const port = 3000;
 const publicDirectoryPath = path.join(__dirname, "../", "public");
 
-// home route
+// handlebars view engine setup
+app.set("view engine", "hbs");
+
 app.use(express.static(publicDirectoryPath));
+
+// view file
+app.get("/", (req, res) => {
+	res.render("index", {
+		title: "Weather App",
+	});
+});
 
 app.get("/weather", (req, res) => {
 	res.send({
