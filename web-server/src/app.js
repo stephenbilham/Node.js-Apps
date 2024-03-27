73485701ce7f6e69ca245147355633765866e457
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-03-27 14:39:57 -0700

    add partials and routing to pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,24 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 const port = 3000;
 
 // Setup paths
-const publicDirectoryPath = path.join(__dirname, "../", "public");
-const viewsPath = path.join(__dirname, "../", "templates");
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // handlebars view engine setup
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
+// set up static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-// view -- set up static directory to serve
+// routes
 app.get("/", (req, res) => {
 	res.render("index", {
 		title: "Weather App",
@@ -25,6 +29,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
 	res.render("about", {
 		title: "About Page",
+		name: "Stephen Bilham",
 		description: "Hello, I invented the weather. Let me tell you about it.",
 	});
 });
@@ -32,6 +37,7 @@ app.get("/about", (req, res) => {
 app.get("/help", (req, res) => {
 	res.render("help", {
 		title: "Help Page",
+		name: "Stephen Bilham",
 		description: "Am I helping you?",
 	});
 });
