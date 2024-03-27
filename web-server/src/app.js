d7ca930f166323b7b8f455951eb7d5aeed4778b7
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-03-27 14:16:37 -0700

    set up new path for views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,17 +3,36 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
+
+// Setup paths
 const publicDirectoryPath = path.join(__dirname, "../", "public");
+const viewsPath = path.join(__dirname, "../", "templates");
 
 // handlebars view engine setup
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
 
 app.use(express.static(publicDirectoryPath));
 
-// view file
+// view -- set up static directory to serve
 app.get("/", (req, res) => {
 	res.render("index", {
 		title: "Weather App",
+		name: "Stephen Bilham",
+	});
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "About Page",
+		description: "Hello, I invented the weather. Let me tell you about it.",
+	});
+});
+
+app.get("/help", (req, res) => {
+	res.render("help", {
+		title: "Help Page",
+		description: "Am I helping you?",
 	});
 });
 
@@ -24,6 +43,7 @@ app.get("/weather", (req, res) => {
 	});
 });
 
+// start server
 app.listen(port, () => {
 	console.log("Listening on port", port);
 });
