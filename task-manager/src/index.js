9d4fb8baa56dd25c4b5ddfcf517e34c980c5a658
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-04-05 11:56:17 -0700

    set up all crud endpoints for users and tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,6 +8,30 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get("/users", async (req, res) => {
+	try {
+		const users = await User.find({});
+		res.send(users).status(201);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+app.get("/users/:id", async (req, res) => {
+	try {
+		const userId = req.params.id;
+
+		const user = await User.findById(userId);
+		if (!user) {
+			return res.status(404).send("User not found!");
+		}
+
+		res.status(200).send(user);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
 app.post("/users", async (req, res) => {
 	try {
 		const user = new User(req.body);
@@ -18,11 +42,100 @@ app.post("/users", async (req, res) => {
 	}
 });
 
+app.put("/users/:id", async (req, res) => {
+	try {
+		const userId = req.params.id;
+		const user = await User.findByIdAndUpdate(userId, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!user) {
+			return res.status(404).send("User not found!");
+		}
+
+		res.status(200).send(user);
+	} catch (e) {
+		res.status(404).send("User not found!");
+	}
+});
+
+app.delete("/users/:id", async (req, res) => {
+	try {
+		const userId = req.params.id;
+		const user = await User.findByIdAndRemove(userId);
+
+		if (!user) {
+			return res.status(404).send("User not found!");
+		}
+
+		res.status(200).send("User successfully removed!");
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
 app.post("/tasks", async (req, res) => {
 	try {
 		const task = new Task(req.body);
 		await task.save();
 		res.send(task).status(201);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+app.get("/tasks", async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.send(tasks).status(201);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+app.put("/tasks/:id", async (req, res) => {
+	try {
+		const taskId = req.params.id;
+		const task = await Task.findByIdAndUpdate(taskId, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!task) {
+			return res.status(404).send("Task not found!");
+		}
+
+		res.status(200).send(task);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+	try {
+		const taskId = req.params.id;
+		const task = await Task.findByIdAndRemove(taskId);
+
+		if (!task) {
+			return res.status(404).send("Task not found!");
+		}
+
+		res.status(200).send("Task successfully removed!");
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
+app.get("/tasks/:id", async (req, res) => {
+	try {
+		const taskId = req.params.id;
+		const task = await Task.findById(taskId);
+
+		if (!task) {
+			return res.status(404).send("Task not found!");
+		}
+
+		res.status(200).send(task);
 	} catch (e) {
 		res.status(400).send(e);
 	}
